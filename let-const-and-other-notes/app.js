58d8cbd548f7600aa3eab95a45e6f971c170e8c7
essssss <srmcnamara7@gmail.com>
2023-02-02 12:29:32 -0500

    exercise on let, const, and var

---
+++ let-const-and-other-notes/app.js
@@ -0,0 +1,13 @@
+// Playing with block scope
+
+// const PI = 3.14;
+// if (true) {
+//    const PI = 45;
+//    console.log(PI);
+// }
+// console.log(PI);
+
+//Hoisting??
+console.log(chickens);
+var chickens = ['butters', 'nancy', 'junior', 'tilda', 'stevie chicks'];
+console.log(chickens);
